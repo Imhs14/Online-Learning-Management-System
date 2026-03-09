fe8ef30d4c4dda64d4b0e1c5e04f57969ebf11ba
imhs14 <heerashanker0214@gmail.com>
2026-03-09 13:56:26 +0530

    commit

--- OnlineLearningManagementSystem/skillmate/settings.py
+++ OnlineLearningManagementSystem/skillmate/settings.py
@@ -204,7 +204,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'sudhatestmail@gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = 'nhbjssyxvxbendzz'
+EMAIL_HOST_PASSWORD = 'nhbjssyxvxbendzz' 
 
 # Cloudinary Django Integration
 
